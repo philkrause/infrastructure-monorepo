7b5b00ce00fee5e53790234ae55b119ad810d285
Philip Krause <philipkrause@Philips-Air.localdomain>
2025-10-01 14:25:34 -0500

    updated module path

--- terraform/projects/shared-infrastructure/dev/gcs-bucket.tf
+++ terraform/projects/shared-infrastructure/dev/gcs-bucket.tf
@@ -1,5 +1,5 @@
 module "gcs_bucket" {
-  source      = "../../modules/shared-infrastructure/gcs-bucket"
+  source      = "../../../modules/shared-infrastructure/gcs-bucket"
   project_id  = var.project_id
   region      = "us-central1"
   environment = "dev"
